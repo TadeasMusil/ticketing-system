2b29bf0d1bf2ba74e5d48009b65be63cc3bac90c
Tadeas <musil.tadeas@gmail.com>
2020-02-13 15:17:01 +0100

    Fix ticket creation form security

--- src/main/java/tadeas_musil/ticketing_system/security/SecurityConfig.java
+++ src/main/java/tadeas_musil/ticketing_system/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
         .authorizeRequests()
         .expressionHandler(webExpressionHandler())
+        .antMatchers("/ticket").permitAll()
         .antMatchers("/cannedResponse/**", "/user/**", "/ticket/**", "/activity/**").authenticated()
         .anyRequest()
         .permitAll()
