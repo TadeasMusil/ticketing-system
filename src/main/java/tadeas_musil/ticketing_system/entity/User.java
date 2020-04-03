cf9018445bf85f7ca448d73b2d9e3815aef02015
Tadeas <musil.tadeas@gmail.com>
2020-04-03 13:56:31 +0200

    Fix missing validation interface

--- src/main/java/tadeas_musil/ticketing_system/entity/User.java
+++ src/main/java/tadeas_musil/ticketing_system/entity/User.java
@@ -18,17 +18,18 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.NaturalId;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import tadeas_musil.ticketing_system.entity.User.Registration;
+import tadeas_musil.ticketing_system.entity.User.ChangePassword;
 import tadeas_musil.ticketing_system.validation.PasswordMatch;
 import tadeas_musil.ticketing_system.validation.UniqueUsername;
-
-
 
 @Getter
 @Setter
@@ -36,51 +37,52 @@ import tadeas_musil.ticketing_system.validation.UniqueUsername;
 @NoArgsConstructor
 @Entity
 @Table(name = "app_user")
-@PasswordMatch(groups = { Registration.class }, first = "password", second = "passwordConfirmation")
+@PasswordMatch(groups = { Registration.class,
+                ChangePassword.class }, first = "password", second = "passwordConfirmation")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
-    
-    public interface Registration {}
 
-    public interface UpdateStaffMember {}
+        public interface Registration {
+        }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private Long id;
+        public interface UpdateStaffMember {
+        }
 
-    @NotBlank(message = "Enter email", groups = { Registration.class })
-    @Email(message = "Invalid email format", groups = { Registration.class })
-    @UniqueUsername(groups = { Registration.class })
-    @NaturalId
-    @EqualsAndHashCode.Include
-    private String username;
+        public interface ChangePassword {
+        }
 
-    @NotBlank(message = "Enter first name", groups = { Registration.class, UpdateStaffMember.class })
-    private String firstName;
-    
-    @NotBlank(message = "Enter last name", groups = { Registration.class, UpdateStaffMember.class })
-    private String lastName;
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE)
+        private Long id;
 
-    @NotBlank(message = "Enter password", groups = { Registration.class })
-    @Size(min = 8, message = "Password must have at least 8 characters", groups = { Registration.class })
-    private String password;
+        @NotBlank(message = "Enter email", groups = { Registration.class })
+        @Email(message = "Invalid email format", groups = { Registration.class })
+        @UniqueUsername(groups = { Registration.class })
+        @NaturalId
+        @EqualsAndHashCode.Include
+        private String username;
 
-    @Transient
-    private String passwordConfirmation;
+        @NotBlank(message = "Enter first name", groups = { Registration.class, UpdateStaffMember.class })
+        private String firstName;
 
-    private boolean isDisabled = false;
-                    
-    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinTable(name = "app_user_role",
-                joinColumns = @JoinColumn(name = "user_id"),
-                inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+        @NotBlank(message = "Enter last name", groups = { Registration.class, UpdateStaffMember.class })
+        private String lastName;
 
-    @ManyToMany(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
-    @JoinTable(name = "app_user_department",
-                joinColumns = @JoinColumn(name = "app_user_id"),
-                inverseJoinColumns = @JoinColumn(name = "department_name"))
-    private Set<Department> departments = new HashSet<>();
+        @NotBlank(message = "Enter password", groups = { Registration.class, ChangePassword.class })
+        @Size(min = 8, message = "Password must have at least 8 characters", groups = { Registration.class,
+                        ChangePassword.class })
+        private String password;
 
-        
+        @Transient
+        private String passwordConfirmation;
+
+        private boolean isDisabled = false;
+
+        @ManyToMany()
+        @JoinTable(name = "app_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+        private Set<Role> roles = new HashSet<>();
+
+        @ManyToMany(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+        @JoinTable(name = "app_user_department", joinColumns = @JoinColumn(name = "app_user_id"), inverseJoinColumns = @JoinColumn(name = "department_name"))
+        private Set<Department> departments = new HashSet<>();
 }
